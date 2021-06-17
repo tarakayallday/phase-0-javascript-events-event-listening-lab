5b54026400e1e5b79fce5561940b9242fc3a56cf
Tara Kelly <tara.kelly16@gmail.com>
2021-06-17 12:21:58 -0400

    added listeners

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function addingEventListener() {
     const input = document.getElementById('input');
+    const main = document.getElementById('main');
+    const three = document.getElementById("three");
 
     function clickAlert() {
         alert('I was clicked!');
     }
+    function mainAlert() {
+        alert('Hello beautiful!');
+    }
 
-    input.addEventListener('click', clickAlert); 
+    input.addEventListener('click', clickAlert);
+    main.addEventListener('click', mainAlert); 
+    three.addEventListener('click', clickAlert);
 }
 addingEventListener();
 
